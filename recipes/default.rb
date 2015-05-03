bd76ad0b1b27ae5c3098f22dc2805b6cd7b072f8
Sean Abrahams <abrahams@gmail.com>
2015-05-03 12:09:04 -0700

    Set cron env from OpsWorks env vars.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,10 +7,11 @@
 
 node[:cron_jobs].each do |cron_values|
   cron "#{cron_values[:name]}" do
-    minute  "#{cron_values[:minute]}"
-    hour    "#{cron_values[:hour]}"
-    day     "#{cron_values[:day]}"
-    month   "#{cron_values[:month]}"
+    environment node[:deploy][:environment_variables]
+    minute "#{cron_values[:minute]}"
+    hour "#{cron_values[:hour]}"
+    day "#{cron_values[:day]}"
+    month "#{cron_values[:month]}"
     weekday "#{cron_values[:weekday]}"
     command "#{cron_values[:command]}"
   end
