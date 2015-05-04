9751c3c670cf60ca3405be92ae0897157adfb6a2
Sean Abrahams <abrahams@gmail.com>
2015-05-04 12:16:14 -0700

    Point to the correct location for env vars and add /usr/local/bin to path

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,9 +5,11 @@
 #
 # Create custom cron jobs
 
+cron_env = Marshal.load(Marshal.dump(node["deploy"]["PTM"]["environment_variables"].to_hash))
+cron_env["PATH"] = "/usr/local/bin:$PATH"
 node[:cron_jobs].each do |cron_values|
   cron "#{cron_values[:name]}" do
-    environment node[:deploy][:environment_variables]
+    environment cron_env
     minute "#{cron_values[:minute]}"
     hour "#{cron_values[:hour]}"
     day "#{cron_values[:day]}"
